295e4045340125d6810742571dad6d4e46462749
arnoson <omasocke@hotmail.de>
2021-06-30 19:15:46 +0200

    Refactor

--- page-src/adsr.js
+++ page-src/adsr.js
@@ -4,19 +4,28 @@ import { linearRamp, chainable } from './utils'
 import { scale, range } from './transforms'
 
 class Envelope {
+  active = false
+  noteOn = false
+  /** @type {number|null} */
+  gateDuration = null
+  /** @type {number|null} */
+  startTime = null
+
   constructor({ a, d, s, r }) {
     this.a = a
     this.d = d
     this.s = s
     this.r = r
-
-    this.noteOn = true
-    this.gateDuration = null
-    this.startTime = null
   }
 
   trigger() {
     this.startTime = null
+    this.noteOn = true
+    this.active = true
+  }
+
+  stop() {
+    this.noteOn = false
   }
 
   /**
@@ -24,6 +33,8 @@ class Envelope {
    * @returns {number}
    */
   value(time) {
+    if (!this.active) return 0
+
     this.startTime ??= time
     const elapsedTime = time - this.startTime
     const { a, d, s, r } = this
@@ -43,6 +54,10 @@ class Envelope {
       // Release
       this.gateDuration ??= elapsedTime
       const factor = Math.min(1, (elapsedTime - this.gateDuration) / r)
+
+      // Envelope has finished.
+      if (factor === 1) this.active = false
+
       // If there was no sustain, there also was no decay so we can start the
       // release at 1.0
       const from = s || 1
@@ -51,16 +66,16 @@ class Envelope {
   }
 }
 
-const envelopes = {}
+export const envelopes = {}
 
 /**
  * Adsr is chainable to `note()`. It creates an envelope and returns a chainable
  * function that returns the envelope value at a given time.
- * @param {*} note
+ * @param {number} note
  * @returns
  */
 export const adsr = note => fn => (a = 100, d = 100, s = 1, r = 100) => {
-  envelopes[note] ??= new Envelope({ a, d, s, r })
+  envelopes[note] = new Envelope({ a, d, s, r })
   const envelope = envelopes[note]
 
   return chainable(({ time }) => envelope.value(time * 1000), { scale, range })
