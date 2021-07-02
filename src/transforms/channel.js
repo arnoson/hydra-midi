8033b9006f773ebd87059e19829f5210fc6b13b7
arnoson <omasocke@hotmail.de>
2021-07-02 18:36:45 +0200

    Allow `note()` and `cc()` to override args

--- src/transforms/channel.js
+++ src/transforms/channel.js
@@ -3,6 +3,9 @@
 import { cc, note } from '../hydra-api'
 
 export const channel = (channel, input = null) => ({
-  note: (_note, _channel, _input) => note(_note, channel, input ?? _input),
-  cc: (_index, _channel, _input) => cc(_index, channel, input ?? _input)
+  note: (_note, _channel, _input) =>
+    note(_note, _channel ?? channel, _input ?? input),
+
+  cc: (_index, _channel, _input) =>
+    cc(_index, _channel ?? channel, _input ?? input)
 })
--- src/transforms/input.js
+++ src/transforms/input.js
@@ -4,7 +4,7 @@ import { cc, note } from '../hydra-api'
 import { channel } from './channel'
 
 export const input = input => ({
-  note: (_note, _channel, _input) => note(_note, _channel, input),
-  cc: (_index, _channel, _input) => cc(_index, _channel, input),
+  note: (_note, _channel, _input) => note(_note, _channel, _input ?? input),
+  cc: (_index, _channel, _input) => cc(_index, _channel, _input ?? input),
   channel: _channel => channel(_channel, input)
 })
