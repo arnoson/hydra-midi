f119bb1895facd18ee914484f79a4b6fba54f76f
arnoson <hallo@arno-schlipf.info>
2023-02-09 12:06:45 +0100

    chore: minify dist

--- rollup.config.js
+++ rollup.config.js
@@ -14,5 +14,5 @@ export default defineConfig({
     format: 'iife',
     file: 'dist/index.js',
   },
-  plugins: [nodeResolve(), string({ include: '**/*.css' })],
+  plugins: [nodeResolve(), terser(), string({ include: '**/*.css' })],
 })
