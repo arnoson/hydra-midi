c1549d5fcf12f1b2efd20a58f105de143f72e2f0
arnoson <hallo@arno-schlipf.info>
2023-02-28 13:18:23 +0100

    fix: show monitor when midi access is ready

--- src/hydra-api/midi.ts
+++ src/hydra-api/midi.ts
@@ -4,17 +4,24 @@ import { show, hide, showInputs } from '../gui'
 import state from '../state'
 import { Defaults } from '../types'
 
-const start = (defaults: Defaults) => {
-  state.defaults = { ...state.initialDefaults, ...defaults }
+type ChainablePromise<T, M> = Promise<T> & M
 
-  midiAccess.start().then(() =>
+const start = (defaults: Defaults) => {
+  state.defaults = { ...state.defaults, ...defaults }
+
+  const promise = midiAccess.start().then(() => {
     midiAccess.access?.addEventListener('statechange', () => {
       if (midiAccess.access) showInputs(midiAccess.access.inputs)
+      if (autoShow) show()
     })
-  )
+  }) as ChainablePromise<void, { show: () => {} }>
 
-  // Allow `midi.start().show()` chaining.
-  return { show }
+  // We can't show the midi monitor directly, since the midi access is probably
+  // not ready yet. Instead we set a flag and check for it in the listener
+  // above.
+  let autoShow = false
+  promise.show = () => (autoShow = true)
+  return promise
 }
 
 const pause = () => midiAccess.pause()
